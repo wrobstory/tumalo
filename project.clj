256cd48c9d72b6dade674bb925bad41fefb7605c
Rob Story <wrobstory@gmail.com>
2016-05-20 14:21:50 -0700

    ENH: First commit, project setup

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject tumalo "0.1.0-SNAPSHOT"
+  :description "Clojure Elasticsearch Indexing Tools"
+  :license {:name "MIT"
+            :url "https://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [clojurewerkz/elastisch "2.2.1"]])
