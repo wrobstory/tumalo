fee3abf1acdb51bcd729956a3c8ceb862e1d0481
Rob Story <wrobstory@gmail.com>
2016-06-08 22:51:40 -0700

    ENH: Add start of s3 indexing logic along with new deps

--- project.clj
+++ project.clj
@@ -4,7 +4,9 @@
             :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.logging "0.3.1"]
+                 [org.clojure/core.async "0.2.374"]
                  [ch.qos.logback/logback-classic "1.1.3"]
+                 [amazonica "0.3.59"]
                  [clojurewerkz/elastisch "2.2.1"]
                  [prismatic/schema "1.1.1"]
                  [clj-http "2.2.0"]
@@ -12,4 +14,5 @@
   :resource-paths ["resources"]
   :profiles {:dev {:resource-paths ["resources/test"]}
              :uberjar {:aot :all}}
-  :global-vars {*warn-on-reflection* true})
+  :global-vars {*warn-on-reflection* true}
+  :javac-options ["-target" "1.8" "-source" "1.8"])
